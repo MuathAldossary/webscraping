61f0ea41a4274f724f6eb75ab1f182b0036771df
Muath Aldossary <114105328+MuathAldossary@users.noreply.github.com>
2024-12-02 11:50:30 +0000

    Add files via upload

---
+++ webScraping/main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+movies = soup.find_all('h3', class_='title')
+moviesList = []
+
+
+for movie in movies:
+    moviesList.append(movie.text)
+
+movies =moviesList.reverse()
+
+file = open('movies.txt', 'w')
+for movie in moviesList:
+    file.write(movie+'\n')
+
+file.close()
